d162948d618dea10e7fbcb69059bd67c4abc4934
Tiago Matos <tiago@tiagomatos.com>
2024-01-03 10:48:47 +1100

    Instalado e configurado Pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import App from './App.vue'
 import router from './router';
 import { createApp } from 'vue'
 import vuetify from './plugins/vuetify';
+import pinia from './plugins/pinia'
 import '@/scss/style.scss';
 import './plugins/yup';
 import './plugins/axios'
@@ -9,5 +10,6 @@ import './plugins/axios'
 const app = createApp(App)
 app
   .use(router)
+  .use(pinia)
   .use(vuetify)
   .mount('#app')
