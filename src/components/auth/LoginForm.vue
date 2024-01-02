48e20da99116b29da1ecf907d46ef71d4ff565d2
Tiago Matos <tiago@tiagomatos.com>
2024-01-02 16:56:42 +1100

    Instalado e configurado VeeValidate

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,14 +1,23 @@
 <template>
   <v-alert v-if="feedbackMessage" color="error" class="mb-2">{{ feedbackMessage }}</v-alert>
-  <v-form @submit.prevent="login">
+  <v-form @submit.prevent="submit">
     <v-row class="d-flex mb-3">
       <v-col cols="12">
         <v-label class="font-weight-bold mb-1">E-mail</v-label>
-        <v-text-field v-model="email" :rules="[rules.required, rules.email]" variant="outlined" color="primary"></v-text-field>
+        <v-text-field
+          v-model="email"
+          :error-messages="errors.email"
+          variant="outlined"
+          color="primary" />
       </v-col>
       <v-col cols="12">
         <v-label class="font-weight-bold mb-1">Senha</v-label>
-        <v-text-field v-model="password" :rules="[rules.required]" variant="outlined" type="password" color="primary"></v-text-field>
+        <v-text-field
+          v-model="password"
+          :error-messages="errors.password"
+          variant="outlined"
+          type="password"
+          color="primary" />
       </v-col>
       <v-col cols="12" class="pt-0">
         <div class="d-flex flex-wrap align-center ml-n2">
@@ -29,22 +38,37 @@
 <script setup>
 import axios from 'axios'
 import {ref} from 'vue';
+import { useForm, useField } from "vee-validate";
+
+const schema = {
+  email(value) {
+    if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+    return 'Must be a valid e-mail.'
+  },
+  password(value) {
+    if (value) return true
+
+    return 'Password is required.'
+  },
+}
+
+const { handleSubmit, errors } = useForm({
+  validationSchema: schema
+})
+
+const submit = handleSubmit((values) => {
+  console.log(values)
+})
+
+const { value: email } = useField('email')
+const { value: password } = useField('password')
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-const email = ref('');
-const password = ref('');
 const feedbackMessage = ref('')
 const loading = ref(false)
-
-const rules = {
-  required: value => !!value || 'Required.',
-  email: value => {
-    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return pattern.test(value) || 'Invalid e-mail.'
-  },
-}
 
 function login() {
   loading.value = true
