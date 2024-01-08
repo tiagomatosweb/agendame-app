fc34e32b8ed6bffcec26464407b2f66246c07f8a
Tiago Matos <tiago@tiagomatos.com>
2024-01-08 11:12:06 +1100

    Redirecionando usuário não logado

--- src/router/MainRoutes.js
+++ src/router/MainRoutes.js
@@ -1,11 +1,22 @@
+import { useMeStore } from '@/store/me';
+
+
 export default {
-    path: '/',
-    component: () => import('@/layouts/full/FullLayout.vue'),
-    children: [
-        {
-            name: 'dashboard',
-            path: '/',
-            component: () => import('@/views/dashboard/index.vue')
-        },
-    ]
+  path: '/',
+  component: () => import('@/layouts/full/FullLayout.vue'),
+  beforeEnter: (to, from, next) => {
+    const meStore = useMeStore();
+    if (!meStore.isLoggedIn) {
+      next({ name: 'login' })
+    } else {
+      next();
+    }
+  },
+  children: [
+    {
+      name: 'dashboard',
+      path: '/',
+      component: () => import('@/views/dashboard/index.vue')
+    },
+  ]
 };
