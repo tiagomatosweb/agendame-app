e2baace87de2bba7fd32756d926ea1f7e18e6365
Tiago Matos <tiago@tiagomatos.com>
2024-02-20 10:40:22 +1100

    Configurando mensagens de erro

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,11 @@
 const messages = {
   ServerErrorException: 'Alguma coisa saiu errado!',
   InvalidAuthenticationException: 'Seu e-mail e/ou senha estão inválidos.',
+  InvalidPasswordResetTokenException: 'Parece que este token está inválido.',
+  InvalidEmailVerifyTokenException: 'Parece que este token está inválido.',
+  UserAlreadyVerifiedException: 'Este usuário já está verificado.',
+  UserHasBeenTakenException: 'Este usuário já existe.',
+  UserNotFoundException: 'Usuário não encontrado.'
 }
 
 export const errorMessage = (code = 'ServerErrorException') => messages[code]
